a1dd442d9f2d5ddb34771908b2fe81dd82469d90
AVZa-BL <83096102+AVZa-BL@users.noreply.github.com>
2026-09-10 16:32:08 +0200

    Add governed hybrid subscription mart

---
+++ game_analytics/models/hybrid_subscription/marts/fct_hybrid_subscription__player_behavior_28d.sql
@@ -0,0 +1,19 @@
+select
+    player_id || '__' || analysis_period as player_period_id,
+    player_id,
+    analysis_period,
+    period_start_at_utc,
+    period_end_at_utc,
+    prior_payer_status,
+    platform,
+    acquisition_channel,
+    is_subscriber,
+    has_eligible_exposure,
+    session_count,
+    session_duration_seconds,
+    liveops_participation_count,
+    subscription_net_revenue_usd,
+    standalone_store_net_revenue_usd,
+    total_net_revenue_usd,
+    discount_amount_usd
+from {{ ref('int_hybrid_subscription__player_28d_behavior') }}
