bd44643092a3f21b8aacb6576ffc1d192c30da7b
AVZa-BL <83096102+AVZa-BL@users.noreply.github.com>
2026-09-10 16:38:26 +0200

    Remove publication separator from game_analytics/tests/hybrid_subscription/assert_player_behavior_has_one_row_per_period.sql

--- game_analytics/tests/hybrid_subscription/assert_player_behavior_has_one_row_per_period.sql
+++ game_analytics/tests/hybrid_subscription/assert_player_behavior_has_one_row_per_period.sql
@@ -1,4 +1,4 @@
-select player_id, analysis_period, count(*) as row_count
+select player_id, analysis_period, count(*) as row_count
 from {{ ref('fct_hybrid_subscription__player_behavior_28d') }}
 group by player_id, analysis_period
 having count(*) != 1
