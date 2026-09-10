1a797afde4e8d75323129a76628161dc836cec4a
AVZa-BL <83096102+AVZa-BL@users.noreply.github.com>
2026-09-10 17:15:06 +0200

    Distinguish clear hybrid incident controls

--- game_analytics/tests/hybrid_subscription/assert_expected_hybrid_incidents_detected.sql
+++ game_analytics/tests/hybrid_subscription/assert_expected_hybrid_incidents_detected.sql
@@ -1,17 +1,18 @@
 with expected as (
     select *
     from (values
-        ('duplicate_store_webhook'),
-        ('mixed_timestamp_mismatch'),
-        ('missing_subscription_grant_link'),
-        ('post_subscription_exposure'),
-        ('cancellation_pending_expiry')
-    ) as required(incident_code)
+        ('duplicate_store_webhook', true),
+        ('mixed_timestamp_mismatch', false),
+        ('missing_subscription_grant_link', true),
+        ('post_subscription_exposure', true),
+        ('cancellation_pending_expiry', true)
+    ) as required(incident_code, requires_affected_rows)
 )
 select expected.incident_code
 from expected
 left join {{ ref('mart_hybrid_subscription__data_quality_incidents') }} incident
     using (incident_code)
 where incident.incident_code is null
-    or incident.affected_rows <= 0
-    or incident.decision_status != 'contained'
+    or (expected.requires_affected_rows and incident.affected_rows <= 0)
+    or (incident.affected_rows > 0 and incident.decision_status != 'contained')
+    or (incident.affected_rows = 0 and incident.decision_status != 'clear')
